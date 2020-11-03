5f3bb565cbf56c83beffbb0da649e62f021a4918
Mardônio Vieira <mardoniovf@gmail.com>
2020-11-03 10:10:33 -0300

    Mostrando dados e imagem do cliente para pagina de profile

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
     baseUrl: "http://localhost:8080",
-    backetBaseUrl: "https://spring-cursomc.s3.sa-east-1.amazonaws.com"
+    bucketBaseUrl: "https://spring-cursomc.s3.sa-east-1.amazonaws.com"
 }
