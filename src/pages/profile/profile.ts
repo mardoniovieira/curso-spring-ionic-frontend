95deaa9e590773fb30cc802d7a1bfde8fac41162
Mardônio Vieira <mardoniovf@gmail.com>
2020-11-03 10:47:47 -0300

    Tratando erros 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -31,8 +31,13 @@ export class ProfilePage {
           this.getImageIfExists();
         },
         error => {
-
+          if(error.status == 403){
+            this.navCtrl.setRoot('HomePage');
+          }
         });
+    }
+    else{
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
